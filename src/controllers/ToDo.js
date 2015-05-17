196992e0d59590c65081eeedcb1b4e664b61fed8
Everett Leo <ejl1952@rit.edu>
2015-05-17 15:49:36 -0400

    update return JSON

--- src/controllers/ToDo.js
+++ src/controllers/ToDo.js
@@ -117,7 +117,7 @@ var makeiosToDo = function(req, res){
 			return res.status(400).json({error:'An error occurred'});
 		}
 		
-		res.json({staus: 'Done'});
+		res.json({'staus': 'Done'});
 	});
 };
 
